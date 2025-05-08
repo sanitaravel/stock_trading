d800c6ab3ee00c8e13b71dd4f4cd2b71de2ddcbf
sanitaravel <alexander.koshcheev.msk@gmail.com>
2025-05-09 00:42:33 +0200

    Update Heroku settings for PostgreSQL configuration and remove unused settings files

--- stock_trading/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('stock-logic/', include('stock_logic.urls')),
-    path('stock-visualization/', include('stock_visualization.urls')),
-]
